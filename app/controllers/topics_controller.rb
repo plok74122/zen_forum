ec21b19acb64ee78163ef7db5b98a49a68fb8838
plok74122 <plok74122@gmail.com>
2016-09-17 14:34:42 +0800

    tags

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,175 +1,175 @@
 class TopicsController < ApplicationController
-	before_action :authenticate_user!, :except=>[:index,:show,:about]
-    before_action :check_author,:only=>[:edit,:update,:destroy]
-	def index
-        @topics = Topic.all
-        @topics.each do |topic|
-        	topic.comments_number=topic.comments.where(:status_published => :true).count
-        	topic.comments_lastest_time=topic.comments.where(:status_published => :true).last.try(:created_at)
-        	topic.save
-        end
+  before_action :authenticate_user!, :except => [:index, :show, :about]
+  before_action :check_author, :only => [:edit, :update, :destroy]
 
-      if params[:category]
-		    @topics = Category.find_by_name(params[:category]).topics
-		# elsif params[:category]=="technical"
-		#     @topics = Category.find(2).topics
-		# elsif params[:category]=="psychological"
-		#     @topics = Category.find(3).topics
-			else
-		    @topics=Topic.all 
-			end
+  def index
+    @topics = Topic.all
+    @topics.each do |topic|
+      topic.comments_number=topic.comments.where(:status_published => :true).count
+      topic.comments_lastest_time=topic.comments.where(:status_published => :true).last.try(:created_at)
+      topic.save
+    end
 
-
-        if params[:order]     
-        # == 'comments_number'
-        	sort_by = "#{params[:order]} Desc"
-        # elsif params[:order] == 'comments_lastest_time'
-        # 	sort_by ='comments_lastest_time Desc'
-        # elsif params[:order] == 'visitingnumber'
-        # 	sort_by ='visitingnumber Desc'
-        else
-        	sort_by ='created_at'
-        end
+    if params[:category]
+      @topics = Category.find_by_name(params[:category]).topics
+      # elsif params[:category]=="technical"
+      #     @topics = Category.find(2).topics
+      # elsif params[:category]=="psychological"
+      #     @topics = Category.find(3).topics
+    else
+      @topics=Topic.all
+    end
 
 
+    if params[:order]
+      # == 'comments_number'
+      sort_by = "#{params[:order]} Desc"
+      # elsif params[:order] == 'comments_lastest_time'
+      # 	sort_by ='comments_lastest_time Desc'
+      # elsif params[:order] == 'visitingnumber'
+      # 	sort_by ='visitingnumber Desc'
+    else
+      sort_by ='created_at'
+    end
 
-		@topics = @topics.where(:status_published => :true).order(sort_by).page(params[:page]).per(5)
 
-		# if current_user
-		# 	@user=current_user
-		# end
+    @topics = @topics.where(:status_published => :true).order(sort_by).page(params[:page]).per(5)
 
-	end
+    # if current_user
+    # 	@user=current_user
+    # end
 
-	def show
-			
-		@topic=Topic.find(params[:id])
-		if params[:record]=="viewing"
-			@topic.visitingnumber=@topic.visitingnumber + 1
-			@topic.save
-	   end
-		# @user=@topic.user
+  end
 
-		
-		if params[:comment_id]
-			@topic = Topic.find(params[:id])
-			@comment=@topic.comments.find(params[:comment_id])
-	  else
-			@comment=Comment.new
-	  end
+  def show
+
+    @topic=Topic.find(params[:id])
+    if params[:record]=="viewing"
+      @topic.visitingnumber=@topic.visitingnumber + 1
+      @topic.save
+    end
+    # @user=@topic.user
+
+
+    if params[:comment_id]
+      @topic = Topic.find(params[:id])
+      @comment=@topic.comments.find(params[:comment_id])
+    else
+      @comment=Comment.new
+    end
 
     unless @comment.new_record? || @comment.is_author?(current_user)
-    	flash[:alert] = "This is not your comment!"
-    	redirect_to topic_path(@topic)
-    	return
-    end
-    
-    @like_num = Likeship.where(:topic => @topic).count
-
-	end
-
-	def new
-		@topic=Topic.new
-	end
-
-	def create
-		@topic=Topic.new(params.require(:topic).permit(:title,:content,:status_published,:photo,:category_ids=>[]))
-	    @topic.user=current_user
-	    if @topic.save
-	    	flash[:notice]="新增成功"
-	    	redirect_to topic_path(@topic)
-	    else
-	    	render :action=>:new
-	    end
-	end
-
-	def edit
-	end
-
-	def update
-		if params[:remove_upload_file]=='1'
-       @topic.photo=nil
+      flash[:alert] = "This is not your comment!"
+      redirect_to topic_path(@topic)
+      return
     end
 
-		if @topic.update(params.require(:topic).permit(:title,:content,:photo,:status_published))
-		  flash[:notice]="更新成功"
-		  redirect_to topic_path(@topic)
-		else
-		  render :action=>:edit
-		end
-	end
-
-	def destroy
-		@topic.destroy
-		redirect_to topics_path
-	end
-
-	def about
-		@users=User.all
-		@topics=Topic.all
-		@comments=Comment.all
-	end
-
-	def keep
-		@topic=Topic.find(params[:id])
-		if current_user.keepedTopic?(@topic)
-			current_user.keeptopics.destroy(@topic)
-			# flash[:notice]="你已取消收藏"
-		else
-			current_user.keeptopics <<@topic
-			# flash[:notice]="收藏成功"
-		end
-
-		respond_to do |format|
-			format.js
-		end
-
-	end
-
-	def like
-		@topic=Topic.find(params[:id])
-		if current_user.likedTopic?(@topic)
-			current_user.like_topics.destroy(@topic)
-			# flash[:notice]="你已取消收藏"
-		else
-			current_user.like_topics <<@topic
-			# flash[:notice]="收藏成功"
-		end
-    
     @like_num = Likeship.where(:topic => @topic).count
-		respond_to do |format|
-			format.js
-		end
 
-	end
+  end
 
-	def subscribe
-		@topic=Topic.find(params[:id])
-		if current_user.subscribedTopic?(@topic)
-			current_user.subscribe_topics.destroy(@topic)
-		else
-			current_user.subscribe_topics <<@topic
-		end
+  def new
+    @topic=Topic.new
+  end
 
-		respond_to do |format|
-			format.js
-		end
+  def create
+    @topic=Topic.new(params.require(:topic).permit(:title, :content, :status_published, :photo, :category_ids => [] , :tag_list => []))
+    @topic.user=current_user
+    if @topic.save
+      flash[:notice]="新增成功"
+      redirect_to topic_path(@topic)
+    else
+      render :action => :new
+    end
+  end
 
-	end
+  def edit
+  end
 
-	private
-	def check_author
-		if current_user.admin?
-			@topic = Topic.find_by_id(params[:id])
-		else	
-	    @topic = current_user.topics.find_by_id(params[:id])
+  def update
+    if params[:remove_upload_file]=='1'
+      @topic.photo=nil
+    end
 
-		    unless @topic 
-		  	  flash[:notice]="作者才能執行"
-		    	redirect_to topics_path
-		    	return
-		    end
-	  end 
-	end
+    if @topic.update(params.require(:topic).permit(:title, :content, :photo, :status_published))
+      flash[:notice]="更新成功"
+      redirect_to topic_path(@topic)
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path
+  end
+
+  def about
+    @users=User.all
+    @topics=Topic.all
+    @comments=Comment.all
+  end
+
+  def keep
+    @topic=Topic.find(params[:id])
+    if current_user.keepedTopic?(@topic)
+      current_user.keeptopics.destroy(@topic)
+      # flash[:notice]="你已取消收藏"
+    else
+      current_user.keeptopics <<@topic
+      # flash[:notice]="收藏成功"
+    end
+
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
+  def like
+    @topic=Topic.find(params[:id])
+    if current_user.likedTopic?(@topic)
+      current_user.like_topics.destroy(@topic)
+      # flash[:notice]="你已取消收藏"
+    else
+      current_user.like_topics <<@topic
+      # flash[:notice]="收藏成功"
+    end
+
+    @like_num = Likeship.where(:topic => @topic).count
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
+  def subscribe
+    @topic=Topic.find(params[:id])
+    if current_user.subscribedTopic?(@topic)
+      current_user.subscribe_topics.destroy(@topic)
+    else
+      current_user.subscribe_topics <<@topic
+    end
+
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
+  private
+  def check_author
+    if current_user.admin?
+      @topic = Topic.find_by_id(params[:id])
+    else
+      @topic = current_user.topics.find_by_id(params[:id])
+
+      unless @topic
+        flash[:notice]="作者才能執行"
+        redirect_to topics_path
+        return
+      end
+    end
+  end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,35 +1,35 @@
 class Topic < ApplicationRecord
   validates_presence_of :title
-	belongs_to :user
-	has_many :comments, :dependent=>:destroy
-	has_many :topic_categoryships,:dependent=>:destroy
-	has_many :categorys, :through=>:topic_categoryships
-	has_many :keepships, :dependent=>:destroy
-	has_many :keepusers, :through=>:keepships, :source => :user
-  has_many :likeships, :dependent=>:destroy
-	has_many :like_users, :through=>:likeships, :source => :user
-	has_many :subscribeships, :dependent=>:destroy
-	has_many :subscirbe_users, :through=>:subscribeships, :source => :user
-  has_many :tagships,:dependent=>:destroy
-	has_many :tags, :through=> :tagships
+  belongs_to :user
+  has_many :comments, :dependent => :destroy
+  has_many :topic_categoryships, :dependent => :destroy
+  has_many :categorys, :through => :topic_categoryships
+  has_many :keepships, :dependent => :destroy
+  has_many :keepusers, :through => :keepships, :source => :user
+  has_many :likeships, :dependent => :destroy
+  has_many :like_users, :through => :likeships, :source => :user
+  has_many :subscribeships, :dependent => :destroy
+  has_many :subscirbe_users, :through => :subscribeships, :source => :user
+  has_many :tagships, :dependent => :destroy
+  has_many :tags, :through => :tagships
 
-	# @topic.comments_with_paginate
-	def comments_with_paginate(page)
-		comments.page(page).per(5)
-	end
-
-	def tag_list
-      self.tags.map{ |x| x.name }.join(",")
+  # @topic.comments_with_paginate
+  def comments_with_paginate(page)
+    comments.page(page).per(5)
   end
 
-  def tag_list=(str)
-      ids = str.split(",").map do |tag_name|
-        tag_name.strip!
-        tag = Tag.find_by_name( tag_name ) || Tag.create( :name => tag_name )
-        tag.id
-      end
+  def tag_list
+    self.tags.map { |x| x.name }.join(",")
   end
 
-	has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  def tag_list=(arr)
+    self.tag_ids = arr.map do |tag_name|
+      tag_name.strip!
+      tag = Tag.find_by_name(tag_name) || Tag.create(:name => tag_name)
+      tag.id
+    end
+  end
+
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
